18f18cc38a85e7c9d6d00aebd6775d4f0683bfd8
David Crenin <david.crenin@gmail.com>
2020-01-22 22:32:41 +0100

    test

--- dsdsite/settings.py
+++ dsdsite/settings.py
@@ -123,7 +123,7 @@ EMAIL_USE_TLS = True
 EMAIL_BACKEND = "sendgrid_backend.SendgridBackend"
 
 # # Toggle sandbox mode (when running in DEBUG mode)
-# SENDGRID_SANDBOX_MODE_IN_DEBUG = True
+SENDGRID_SANDBOX_MODE_IN_DEBUG = False
 
 # # echo to stdout or any other file-like object that is passed to the backend via the stream kwarg.
 # SENDGRID_ECHO_TO_STDOUT = True
