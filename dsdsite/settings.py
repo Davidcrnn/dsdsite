4fba0394ab632ef5bfa497c25e98c51a24f4fa64
David Crenin <david.crenin@gmail.com>
2020-01-23 08:43:54 +0100

    test

--- dsdsite/settings.py
+++ dsdsite/settings.py
@@ -14,9 +14,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['ancient-retreat-84146.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
