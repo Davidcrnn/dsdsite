b8cfb4e6f31d6146b2f0bf2add957b397976f63a
David Crenin <david.crenin@gmail.com>
2020-04-24 17:42:21 +0200

    Debug

--- dsdsite/settings.py
+++ dsdsite/settings.py
@@ -14,9 +14,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+if DEBUG:
+    ALLOWED_HOSTS = ['*']
+else:
+    ALLOWED_HOSTS = ['localhost', 'dsigndev.com']
 
 
 # Application definition
@@ -44,8 +47,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
-
 
 
 ROOT_URLCONF = 'dsdsite.urls'
